e5fae82e66695701e580fe3b822827de28584e68
유지상 <digndign2@gmail.com>
2020-09-09 00:01:33 +0900

    링크드 리스트 1-3

--- DataStructure/LinkedList.py
+++ DataStructure/LinkedList.py
@@ -75,10 +75,103 @@ print(node.data)
 
 # 5. 파이썬 객체지향 프로그래밍으로 링크드 리스트 구현하기
 
+
+# class Node:
+#     def __init__(self, data, next=None):
+#         self.data = data
+#         self.next = next
+#
+#
+# class NodeMgmt:
+#     def __init__(self, data):
+#         self.head = Node(data)
+#
+#     def add(self, data):
+#         if self.head == '':
+#             self.head = Node(data)
+#         else:
+#             node = self.head
+#             while node.next:
+#                 node = node.next
+#             node.next = Node(data)
+#
+#     def desc(self):
+#         node = self.head
+#         while node:
+#             print(node.data)
+#             node = node.next
+
+# linkedlist1 = NodeMgmt(0)
+# linkedlist1.desc()
+#
+# for data in range(1, 10):
+#     linkedlist1.add(data)
+#
+# linkedlist1.desc()
+
+# 6. 링크드 리스트의 복잡한 기능2 (특정 노드를 삭제)
+# 다음 코드는 위의 코드에서 delete 메소드만 추가한 것
+#. [1] head 삭제
+#. [2] 마지막 노드 삭제
+#. [3] 중간 노드 삭제
+
+
 class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
 
 class NodeMgmt:
+    def __init__(self, data):
+        self.head = Node(data)
 
     def add(self, data):
+        if self.head == '':
+            self.head = Node(data)
+        else:
+            node = self.head
+            while node.next:
+                node = node.next
+            node.next = Node(data)
 
     def desc(self):
+        node = self.head
+        while node:
+            print(node.data)
+            node = node.next
+
+    def delete(self, data):
+        if self.head == '':
+            print("해당 값을 가진 노드가 없습니다.")
+            return
+
+        if self.head.data == data:
+            temp = self.head
+            self.head = self.head.next
+            del temp
+        else:
+            node = self.head
+            while node.next:
+                if node.next.data == data:
+                    temp = node.next
+                    node.next = node.next.next
+                    del temp
+                else:
+                    node = node.next
+
+print("clear")
+linkedlist1 = NodeMgmt(0)
+linkedlist1.desc()
+
+# print(linkedlist1.head)
+# linkedlist1.delete(0)
+# print(linkedlist1.head)
+
+for data in range(1, 10):
+    linkedlist1.add(data)
+linkedlist1.desc()
+linkedlist1.delete(4)
+linkedlist1.desc()
+linkedlist1.delete(9)
+linkedlist1.desc()
