3aee3ba3445c277d99cb62ba1a423eb78de78b5a
유지상 <digndign2@gmail.com>
2020-09-15 00:45:34 +0900

    해쉬 테이블 4 - Linear Probing

--- DataStructure/HashTable.py
+++ DataStructure/HashTable.py
@@ -204,3 +204,48 @@ print(hash('Data') % 8)
 save_data('Dd', '1201023010')
 save_data('Data', '1201023010')
 read_data('Dd')
+
+# 6.2. Linear Probing 기법
+# * 폐쇄 해슁 또는 Close Hashing 기법 중 하나 : 해쉬 테이블 저장공간 안에서 충돌 문제를 해결하는 기법
+# * 충돌이 일어나면, 해당 hash address의 다음 address 부터 맨 처음 빈공간에 저장하는 기법
+
+
+hash_table = list([0 for i in range(8)])
+
+
+def get_key(data):
+    return hash(data)
+
+
+def hash_function(key):
+    return key % 8
+
+
+def save_data(data,value):
+    index_key = get_key(data)
+    hash_address = hash_function(index_key)
+    if hash_table[hash_address] != 0:
+        for index in range(hash_address, len(hash_table)):
+            if hash_table[index] == 0:
+                hash_table[index] = [index_key, value]
+                return
+            elif hash_table[index][0] == index_key:
+                hash_table[index][1] = value
+                return
+    else:
+        hash_table[hash_address] = [index_key, value]
+
+
+def read_data(data):
+    index_key = get_key(data)
+    hash_address = hash_function(index_key)
+
+    if hash_table[hash_address] != 0:
+        for index in range(hash_address, len(hash_table)):
+            if hash_table[index] == 0:
+                return None
+            elif hash_table[index][0] == index_key:
+                return hash_table[index][1]
+        return None
+    else:
+        return None
