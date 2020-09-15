3c761b8f04d3d20d05916bcec4a9a85a7f9c6ad5
유지상 <digndign2@gmail.com>
2020-09-16 00:49:01 +0900

    트리 3 - 이진 탐색 트리 search 함수 구현

--- DataStructure/Tree.py
+++ DataStructure/Tree.py
@@ -62,7 +62,21 @@ class NodeMgmt:
                     self.current_node.right = Node(value)
                     break
 
+    def search(self, value):
+        self.current_node = self.head
+        while self.current_node:
+            if self.current_node.value == value:
+                return True
+            elif value < self.current_node.value:
+                self.current_node = self.current_node.left
+            else:
+                self.current_node = self.current_node.right
+
+        return False
+
 
 head = Node(1)
 BST = NodeMgmt(head)
 BST.insert(2)
+print(BST.search(2))
+print(BST.search(0))
