0634c2330450dacdb7e61ab26a91de949c57115f
유지상 <digndign2@gmail.com>
2020-10-24 22:08:38 +0900

    힙 - 4 삭제 구현

--- DataStructure/Heap.py
+++ DataStructure/Heap.py
@@ -80,6 +80,65 @@ class Heap:
 
         return True
 
+    def move_down(self, popped_idx):
+        left_child_popped_idx = popped_idx * 2
+        right_child_popped_idx = popped_idx * 2 + 1
+
+        # 1] 자식이 없는 경우
+        if left_child_popped_idx >= len(self.heap_array):
+            return False
+        # 2] 오른쪽 자식 노드만 없을 때
+        elif right_child_popped_idx >= len(self.heap_array):
+            if self.heap_array[popped_idx] < self.heap_array[left_child_popped_idx]:
+                return True
+            else:
+                return False
+        # 3] 자식이 2개 있는 경우 (L, R)
+        else:
+            if self.heap_array[left_child_popped_idx] > self.heap_array[right_child_popped_idx]:
+                if self.heap_array[popped_idx] < self.heap_array[left_child_popped_idx]:
+                    return True
+                else:
+                    return False
+            else:
+                if self.heap_array[popped_idx] < self.heap_array[right_child_popped_idx]:
+                    return True
+                else:
+                    return False
+
+    def pop(self):
+        if len(self.heap_array) <= 1:
+            return None
+
+        returned_data = self.heap_array[1]
+
+        self.heap_array[1] = self.heap_array[-1]
+        del self.heap_array[-1]
+        popped_idx = 1
+
+        while self.move_down(popped_idx):
+            left_child_popped_idx = popped_idx * 2
+            right_child_popped_idx = popped_idx * 2 + 1
+
+            # 2] 오른쪽 자식 노드만 없을때
+            if right_child_popped_idx >= len(self.heap_array):
+                if self.heap_array[popped_idx] < self.heap_array[left_child_popped_idx]:
+                    self.heap_array[popped_idx], self.heap_array[left_child_popped_idx] = self.heap_array[left_child_popped_idx], self.heap_array[popped_idx]
+                    popped_idx = left_child_popped_idx
+            # 3] 왼쪽, 오른쪽 자식 노드 모두 있을 때
+            else:
+                if self.heap_array[left_child_popped_idx] > self.heap_array[right_child_popped_idx]:
+                    if self.heap_array[popped_idx] < self.heap_array[left_child_popped_idx]:
+                        self.heap_array[popped_idx], self.heap_array[left_child_popped_idx] = self.heap_array[left_child_popped_idx], self.heap_array[popped_idx]
+                        popped_idx = left_child_popped_idx
+                else:
+                    if self.heap_array[popped_idx] < self.heap_array[right_child_popped_idx]:
+                        self.heap_array[popped_idx], self.heap_array[right_child_popped_idx] = self.heap_array[right_child_popped_idx], self.heap_array[popped_idx]
+                        popped_idx = right_child_popped_idx
+
+        return returned_data
+
+
 heap = Heap(15)
 heap.insert(10)
 heap.insert(8)
@@ -87,4 +146,11 @@ heap.insert(5)
 heap.insert(4)
 heap.insert(20)
 
+print(heap.pop())
 print(heap.heap_array)
+
+# 5. 힙(Heap) 시간 복잡도
+# depth (트리의 높이) 를 h라고 표기한다면,
+# n개의 노드를 가지는 heap 에 데이터 삽입 또는 삭제시, 최악의 경우 root 노드에서 leaf 노드까지 비교해야 하므로  ℎ=𝑙𝑜𝑔2𝑛  에 가까우므로, 시간 복잡도는  𝑂(𝑙𝑜𝑔𝑛)
+# 참고: 빅오 표기법에서  𝑙𝑜𝑔𝑛  에서의 log의 밑은 10이 아니라, 2입니다.
+# 한번 실행시마다, 50%의 실행할 수도 있는 명령을 제거한다는 의미. 즉 50%의 실행시간을 단축시킬 수 있다는 것을 의미함
