933c4f67d48d8e5495643ff96bf5f2c98c444698
spiritlink <digndign2@gmail.com>
2022-08-10 23:39:22 +0900

    너비 우선 탐색 - BFS 02

--- Algorithm/18_bfs.py
+++ Algorithm/18_bfs.py
@@ -12,3 +12,29 @@ graph['I'] = ['C', 'J']
 graph['J'] = ['I']
 
 print(graph)
+
+# data = [1,2,3]
+# data.extend([4,5])
+# print(data)
+
+
+def bfs(graph, start_node):
+    visited = list()
+    need_visit = list()
+
+    need_visit.append(start_node)
+    count = 0
+
+    while need_visit:
+        count += 1
+        node = need_visit.pop(0)
+        if node not in visited:
+            visited.append(node)
+            need_visit.extend(graph[node])
+
+    print(count)  # O(V + E) [간선 + 노드 수]
+    return visited
+
+
+visit = bfs(graph, 'A')
+print(visit)
