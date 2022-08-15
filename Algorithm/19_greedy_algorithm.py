ec912f469c220bcd0354a10c11eaaa637ea4c1a3
spiritlink <digndign2@gmail.com>
2022-08-15 23:50:01 +0900

    탐욕 알고리즘 02 - 부분 배낭 문제

--- Algorithm/19_greedy_algorithm.py
+++ Algorithm/19_greedy_algorithm.py
@@ -17,3 +17,29 @@ def min_coin_count(value, coin_list):
 
 
 print(min_coin_count(4520, coin_list))
+
+# 부분 배낭 문제 (Fractional Knapsack Problem)
+data_list = [(10, 10), (15, 12), (20, 10), (25, 8), (30, 5)]
+data_list = sorted(data_list, key=lambda x: x[1] / x[0], reverse=True)
+print(data_list)
+
+
+def get_max_value(data_list, capacity):
+    data_list = sorted(data_list, key=lambda x: x[1] / x[0], reverse=True)
+    total_value = 0
+    details = list()
+
+    for data in data_list:
+        if capacity - data[0] >= 0:
+            capacity -= data[0]
+            total_value += data[1]
+            details.append([data[0], data[1], 1])
+        else:
+            fraction = capacity / data[0]
+            capacity -= data[0] * fraction
+            total_value += data[1] * fraction
+            details.append([data[0], data[1], fraction])
+            break
+    return total_value, details
+
+print(get_max_value(data_list, 30))
