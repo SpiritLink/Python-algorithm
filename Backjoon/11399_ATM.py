293a07ef3dd965888dab3a40e53fd8b2ad0c2f25
spiritlink <digndign2@gmail.com>
2022-08-16 23:30:08 +0900

    탐욕 알고리즘 - 백준 11399 ATM 문제

---
+++ Backjoon/11399_ATM.py
@@ -0,0 +1,18 @@
+﻿# 백준에서는 밑과 같이, input으로 읽어야 한다.
+# humanCount = int(input())
+# times = list(map(int, input().split()))
+
+# 예제 입력
+humanCount = 5
+times = [3, 1, 4, 3, 2]
+
+# 예제 출력 32
+times.sort()
+
+total_time = 0
+current = 0
+for time in times:
+    current = current + time
+    total_time = total_time + current
+
+print(total_time)
