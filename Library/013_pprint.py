184f5c0b37a29dc2eea0333297f94db7189c6504
유지상 <spiritlink@neowiz.com>
2022-08-17 12:48:25 +0900

    라이브러리 pprint

---
+++ Library/013_pprint.py
@@ -0,0 +1,8 @@
+﻿import pprint
+#  데이터를 보기 좋게 출력(pretty print) 할 때 사용하는 모듈
+
+result = {'userId': 1, 'id': 1, 'title': 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+          'body': 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae' \
+                  'ut ut quas totam\nnostrum rerum est autem suntrem eveniet architecto'}
+
+pprint.pprint(result)
