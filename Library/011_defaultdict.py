5543fe0c6729505c7272332870ac5ea50d986429
유지상 <spiritlink@neowiz.com>
2022-08-17 09:53:28 +0900

    라이브러리 - defaultdict

---
+++ Library/011_defaultdict.py
@@ -0,0 +1,19 @@
+﻿from collections import defaultdict
+#  초깃값을 지정하여 딕셔너리를 생성하는 모듈
+text = "Life is too short, You need python."
+
+# 일반적인 풀이
+d = dict()
+for key in text:
+    if key not in d:
+        d[key] = 0
+    d[key] += 1
+
+print(d)
+
+# default dict 사용시
+d = defaultdict(int)
+for key in text:
+    d[key] += 1
+
+print(dict(d))
