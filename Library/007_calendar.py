2c43dec4fab36bbf04837cda9719ba5df4af44c9
유지상 <spiritlink@neowiz.com>
2022-08-17 09:27:02 +0900

    라이브러리 - calendar / deque (윤년 계산, 데크)

---
+++ Library/007_calendar.py
@@ -0,0 +1,9 @@
+﻿import calendar
+
+# 윤년인지 확인할 수 있다.
+
+print(calendar.isleap(0))
+print(calendar.isleap(1))
+print(calendar.isleap(4))
+print(calendar.isleap(1200))
+print(calendar.isleap(700))
---
+++ Library/008_deque.py
@@ -0,0 +1,17 @@
+﻿from collections import deque
+
+# 리스트를 n 만큼 회전할때 사용한다.
+
+a = [1, 2, 3, 4, 5]
+q = deque(a)
+q.rotate(2)  # 시계방향은 양수, 그 반대는 음수
+result = list(q)
+print(result)
+
+q.append(10)
+q.appendleft(14)
+q.pop()
+q.popleft()
+
+#  첫번째 요소를 삭제할 때 리스트 보다 데크가 빠르다. O(N) vs O(1) 연산
+#  스레드 환경에서 안전하다
