3b4eb8c14f995d7bfad17a3fef2942573c5608fe
유지상 <spiritlink@neowiz.com>
2022-08-19 09:10:27 +0900

    라이브러리 - Topological Sorter

---
+++ Library/016_TopologicalSorter.py
@@ -0,0 +1,17 @@
+﻿from graphlib import TopologicalSorter
+# 위상 정렬에 사용하는 클래스
+
+ts = TopologicalSorter()
+
+# 규칙 1
+ts.add('영어 중급', '영어 초급')  # 영어 중급의 선수 과목은 영어 초급
+ts.add('영어 고급', '영어 중급')  # 영어 고급의 선수 과목은 영어 중급
+
+# 규칙 2
+ts.add('영어 문법', '영어 중급')  # 영어 문법의 선수 과목은 영어 중급
+ts.add('영어 고급', '영어 문법')  # 영어 고급의 선수 과목은 영어 문법
+
+# 규칙 3
+ts.add('영어 회화', '영어 문법')  # 영어 회화의 선수 과목은 영어 문법
+
+print(list(ts.static_order()))  # 위상 정렬한 결과를 출력
