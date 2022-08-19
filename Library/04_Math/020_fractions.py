bf2deb6a6550305c9f999ddd9dbe5702dfe90006
유지상 <spiritlink@neowiz.com>
2022-08-19 09:35:04 +0900

    라이브러리 - fractions, random (유리수, 랜덤)

---
+++ Library/04_Math/020_fractions.py
@@ -0,0 +1,21 @@
+﻿from fractions import Fraction
+# 유리수를 계산할 때 사용하는 모듈이다.
+
+print(1/5 + 2/5)
+
+# 분자 / 분모 형태로 만들 수 있다.
+a = Fraction(1, 5)
+print(a)
+
+# 분자 / 분모 형태로 만들 수 있다 (2).
+a = Fraction('1/5')
+print(a)
+
+# 분자의 값 확인
+print(a.numerator)
+
+# 분모의 값 확인
+print(a.denominator)
+
+result = Fraction(1, 5) + Fraction(2, 5)
+print(result)
---
+++ Library/04_Math/021_random.py
@@ -0,0 +1,19 @@
+﻿import random
+
+# 1 ~ 45 값중, 무작위로 6개 추출
+result = []
+while len(result) < 6:
+    num = random.randint(1, 45)
+    if num not in result:
+        result.append(num)
+
+print(result)
+
+# 리스트 요소를 무작위로 섞는다. (shuffle)
+a = [1, 2, 3, 4, 5]
+random.shuffle(a)
+print(a)
+
+# 리스트 요소에서 무작위로 하나를 선택한다. (choice)
+a = [1, 2, 3, 4, 5]
+print(random.choice(a))
