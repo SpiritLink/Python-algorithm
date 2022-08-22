174d24fef16c1ce3328eceed2345b27f3532b5d4
유지상 <spiritlink@neowiz.com>
2022-08-22 09:43:55 +0900

    라이브러리 - cycle, accumulate

---
+++ Library/05_FunctionalProgramming/023_cycle.py
@@ -0,0 +1,9 @@
+﻿import itertools
+#  반복 가능한 객체를 순서대로 무한히 반복하는 이터레이터를 생성하는 함수
+emp_pool = itertools.cycle(['김은경', '이명자', '이성진'])
+print(next(emp_pool))
+print(next(emp_pool))
+print(next(emp_pool))
+print(next(emp_pool))
+print(next(emp_pool))
+
---
+++ Library/05_FunctionalProgramming/024_accumulate.py
@@ -0,0 +1,9 @@
+﻿import itertools
+
+monthly_income = [1161, 1814, 1270, 2256, 1413, 1842, 2221, 2207, 2450, 2823, 2540, 2134]
+result = list(itertools.accumulate(monthly_income))
+
+print(result)  # 월별 누적 합계이다.
+
+maxResult = list(itertools.accumulate(monthly_income, max))
+print(maxResult) #  그때까지의 최댓값
