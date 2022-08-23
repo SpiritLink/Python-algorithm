90e0a943ca4bee41044ca4b7e473994f9d207745
spiritlink <digndign2@gmail.com>
2022-08-23 23:31:25 +0900

    프림 알고리즘 - heapq / defaultdict 라이브러리 추가

---
+++ Algorithm/20_graph_prim.py
@@ -0,0 +1,13 @@
+﻿import heapq  # 힙 라이브러리
+from collections import defaultdict  # default dict
+
+# 힙 사용하기
+graph_data = [[2, 'A'], [5, 'B'], [3, 'C']]
+
+heapq.heapify(graph_data)
+
+for index in range(len(graph_data)):
+    print(heapq.heappop(graph_data))
+
+list_dict = defaultdict(list)  # 키값이 없을경우 list를 반환
+print(list_dict['key1'])
