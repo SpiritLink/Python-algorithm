f93954f88d9b169ffef495a475e64fd5bcdfcb4f
유지상 <spiritlink@neowiz.com>
2022-08-24 09:20:27 +0900

    라이브러리 combinations

---
+++ Library/05_FunctionalProgramming/028_combinations.py
@@ -0,0 +1,9 @@
+﻿import itertools
+it = itertools.combinations(range(1, 46), 6)
+
+#  끝도 없이 출력된다.
+# for num in it:
+#     print(num)
+
+print(len(list(itertools.combinations(range(1, 46), 6))))
+# 로또 번호의 가지수는 8145060 개이다.
