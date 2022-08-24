6f5f4f4acdb033a738bda9a9127e89eb804841c7
유지상 <spiritlink@neowiz.com>
2022-08-24 09:08:19 +0900

    라이브러리 zip_longest

---
+++ Library/05_FunctionalProgramming/026_zip_longest.py
@@ -0,0 +1,11 @@
+﻿import itertools
+
+students = ['한민서', '황지민', '이영철', '이광수', '김승민']
+rewards = ['사탕', '초콜릿', '젤리']
+
+result = zip(students, rewards)
+print(list(result))
+
+#  부족한 reward는 새우깡으로 채워 간식을 나누는 코드를 작성해야 한다.
+result = itertools.zip_longest(students, rewards, fillvalue='새우깡')
+print(list(result))
